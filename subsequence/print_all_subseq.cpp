730b801b8a480fe9b74b3ee36833a56527c4d35c
Rohith1221 <62758125+Rohith1221@users.noreply.github.com>
2020-08-16 16:12:40 +0530

    Create print_all_subseq.cpp
    print all the sub sequences of the string .

---
+++ subsequence/print_all_subseq.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print_sub(string input,string output)
+{
+    if(input.length()==0)
+    {
+        cout<<output<<endl;
+        return;
+    }
+    print_sub(input.substr(1),output);
+    print_sub(input.substr(1),output+input[0]);
+}
+
+int main()
+{
+    string input,output="";
+    getline(cin,input);
+    print_sub(input,output);
+    return 0;
+
+}
